20aee98e5defd7ee8b40aa3e66188d035f26eb3a
Imonuil Suleimanov <imasuleymanov@gmail.com>
2022-12-08 22:15:35 +0600

    The javascript for calculating the amount of goods and the cart composition has been created

---
+++ src/cart.js
@@ -0,0 +1,10 @@
+let ShoppingCart = document.getElementById("shopping-cart");
+let label = document.getElementById("label");
+
+/**
+ * ! Basket to hold all the selected items
+ * ? the getItem part is retrieving data from the local storage
+ * ? if local storage is blank, basket becomes an empty array
+ */
+
+ let basket = JSON.parse(localStorage.getItem("data")) || [];
