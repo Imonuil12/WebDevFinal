ba24df84bb34ee06fa8cf995b4cc12a78c3c44b3
Imonuil Suleimanov <imasuleymanov@gmail.com>
2022-12-08 22:20:29 +0600

    The function to calculate the amount of the good and finding out the toal price in the cart

--- src/cart.js
+++ src/cart.js
@@ -19,3 +19,58 @@ let calculation = () => {
   };
   
   calculation();
+
+  /**
+ * ! Generates the Cart Page with product cards composed of
+ * ! images, title, price, buttons, & Total price
+ * ? When basket is blank -> show's Cart is Empty
+ */
+
+let generateCartItems = () => {
+    if (basket.length !== 0) {
+      return (ShoppingCart.innerHTML = basket
+        .map((x) => {
+          let { id, item } = x;
+          let search = shopItemsData.find((x) => x.id === id) || [];
+          let { img, price, name } = search;
+          return `
+        <div class="cart-item">
+          <img width="100" src=${img} alt="" />
+  
+          <div class="details">
+          
+            <div class="title-price-x">
+              <h4 class="title-price">
+                <p>${name}</p>
+                <p class="cart-item-price">$ ${price}</p>
+              </h4>
+              <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
+            </div>
+  
+            <div class="cart-buttons">
+              <div class="buttons">
+                <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                <div id=${id} class="quantity">${item}</div>
+                <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+              </div>
+            </div>
+  
+            <h3>$ ${item * price}</h3>
+          
+          </div>
+        </div>
+        `;
+        })
+        .join(""));
+    } else {
+      ShoppingCart.innerHTML = "";
+      label.innerHTML = `
+      <h2>Cart is Empty</h2>
+      <a href="index.html">
+        <button class="HomeBtn">Back to Home</button>
+      </a>
+      `;
+    }
+  };
+  
+  generateCartItems();
