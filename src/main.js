d71835468ed31ffa02147df6393c225a804d223a
ImonuilSuleimanov <imonuil.suleimanov_2025@ucentralasia.org>
2022-12-10 21:56:50 +0600

    Generates the shop with product cards composed of image, title, price, buttons, description

--- src/main.js
+++ src/main.js
@@ -12,3 +12,33 @@ let basket = JSON.parse(localStorage.getItem("data")) || [];
  * ! Generates the shop with product cards composed of
  * ! images, title, price, buttons, description
  */
+
+ let generateShop = () => {
+    return (shop.innerHTML = shopItemsData
+      .map((x) => {
+        let { id, name, desc, img, price } = x;
+        let search = basket.find((y) => y.id === id) || [];
+        return `
+      <div id=product-id-${id} class="item">
+        <img width="220" src=${img} alt="">
+        <div class="details">
+          <h3>${name}</h3>
+          <p>${desc}</p>
+          <div class="price-quantity">
+            <h2>$ ${price} </h2>
+            <div class="buttons">
+              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+              <div id=${id} class="quantity">${
+          search.item === undefined ? 0 : search.item
+        }</div>
+              <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+            </div>
+          </div>
+        </div>
+    </div>
+      `;
+      })
+      .join(""));
+  };
+  
+  generateShop();
